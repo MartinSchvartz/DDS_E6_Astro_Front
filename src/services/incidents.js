dafb01aa8a85db0cec705f84894837c3c108a4f7
MartinSchvartz <martinschvartz@gmail.com>
2024-02-15 20:09:23 -0300

    Added .env file

--- src/services/incidents.js
+++ src/services/incidents.js
@@ -1,5 +1,5 @@
 export const getIncidents = async () => {
-  const response = await fetch("https://dds-tpa.onrender.com/incidents");
+  const response = await fetch(import.meta.env.PUBLIC_INCIDENTS_API);
   const data = await response.json();
   return data;
 };
